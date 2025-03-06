0c92dbe39b71383cf8cc693fad98a4e19b9c7108
Niels Saurer <me@nielssaurer.com>
2025-03-06 20:23:21 +0100

    add TODO

--- bot-client/src/main.rs
+++ bot-client/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     // TODO: take in a CLI for server addr and amount of bots to spawn,
     // then for every bot spawn a thread that runs that bot.
+
+    // TODO: check memory/CPU usage locally at like 1000 bots and find appropriate server.
+    // also check how distributed the workload is across cores.
     println!("Hello, world!");
 }
